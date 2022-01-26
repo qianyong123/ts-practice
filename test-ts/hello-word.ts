8486d0ad20f70d4cd127fc86dd49bf4e0bf41682
qianyong123 <40485005+qianyong123@users.noreply.github.com>
2022-01-26 15:33:10 +0800

    Update hello-word.ts

--- test-ts/hello-word.ts
+++ test-ts/hello-word.ts
@@ -1,4 +1,13 @@
 // 期望是一个 string 类型
-type HelloWorld = any
-// 你需要使得如下这行不会抛出异常
-type test = Expect<Equal<HelloWorld, string>>
+// type HelloWorld = any
+type HelloWorld = string // expected to be a string
+
+
+/* _____________ 测试用例 _____________ */
+import { Equal, Expect, NotAny } from '@type-challenges/utils'
+
+type cases = [
+  Expect<NotAny<HelloWorld>>,
+  Expect<Equal<HelloWorld, string>>
+]
+
